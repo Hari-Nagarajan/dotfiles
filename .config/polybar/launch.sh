9a1bedb5eb5d5b01d105d447cf1f58db00831469
Hari Nagarajan <39167399+Hari-Nagarajan@users.noreply.github.com>
2021-10-26 14:56:32 -0400

    Update launch.sh

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,6 +6,9 @@ killall -q polybar
 # polybar-msg cmd quit
 
 # Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar top-left 2>&1 | tee -a /tmp/polybar1.log & disown
+# echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+echo "---" | tee -a /tmp/example.log
+polybar --config=$HOME/.config/polybar/config.ini example 2>&1 | tee -a /tmp/example.log & disown
+# polybar bar2 2>&1 | tee -a /tmp/polybar2.log & disown
+
 echo "Bars launched..."
